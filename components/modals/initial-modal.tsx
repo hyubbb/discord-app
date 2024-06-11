cafb09b313542601f026202978c575058e172630
hyubbb <dlckdguq1011@gmail.com>
2024-06-11 21:57:12 +0900

    upload modal

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as z from "zod";
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
@@ -23,7 +24,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
@@ -31,7 +32,7 @@ const formSchema = z.object({
 });
 
 const InitialModal = () => {
-  const [isMouted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
@@ -51,7 +52,7 @@ const InitialModal = () => {
     console.log(values);
   };
 
-  if (!isMouted) return null;
+  if (!isMounted) return null;
 
   return (
     <Dialog open>
@@ -70,7 +71,23 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
             <div className='space-y-8 px-6'>
               <div className='flex items-center justify-center text-center'>
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name='imageUrl'
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormControl>
+                          <FileUpload
+                            endpoint='serverImage'
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    );
+                  }}
+                />
               </div>
               <FormField
                 control={form?.control}
